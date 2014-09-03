2b14c9d85726b2b1db303cee5fb35312d17ea270
Ansel <aesthapus@gmail.com>
2014-09-03 17:43:46 -0400

    ported to qt4. need documentation and dependency referencing

--- testing/README.md
+++ testing/README.md
@@ -1,16 +1,16 @@
 ###Phase Response Curve
 
-**Requirements:** None
-**Limitations:** None
+**Requirements:** None  
+**Limitations:** None  
 
-![EDIT THIS LINK](http://www.rtxi.org/wp-content/uploads/2010/04/PRC.png)
+![PRC GUI](PRC.png)
 
 This module applies an alpha-shaped conductance to the cell at a fixed delay after 10 interspike intervals (ISI). It computes an intrinsic period P0 by averaging the most recent 5 of 10 ISIs. Thus, the value of P0 can change over time. The period after the perturbed period is designated as P1 and the period following P1 is designated as P2. The first and second order PRCs are computed as PRC1=(P1-P0)/P0 and PRC2=(P2-P0)/P0. By this convention, a delay in the spike yield a positive value and an advance yields a negative value. The phase of the perturbation stimulus is computed based on the most recent measured P0. To help you choose the fixed delay for the perturbations, use the “Measure intrinsic P0″ at the top of the module to begin computing a running average of measured ISIs. After you start running the module, the ISI textbox will simply display the current measured ISI. To run a PRC protocol by stepping through the specified fixed delays, click the “Measure PRC” button. Stop either mode of data collection by clicking the “Pause” button. The source code is heavily commented and the program logic is based on indexing the number of spikes that have occurred in each stimulus cycle. It is easy to change how the intrinsic period is computed and whether it is held constant during the entire experiment or recomputed. Similarly, you can easily change the sign convention for computing the phase shifts and extend the code to compute other phase response measures.
 
 This screenshot was made using the Connor Stevens model to generate spikes. A complete demo is available for re-creating this screenshot. 
 
-![EDIT THIS LINK](http://www.rtxi.org/wp-content/uploads/2010/04/PRC-scope.png)
+![PRC and Scope](PRC-scope.png)
 
 This module has a second plotting mode that creates a ts-tr plot of the stimulus times and response times for the perturbed period.
 
-![EDIT THIS LINK](http://www.rtxi.org/wp-content/uploads/2010/04/PRC-tstr.png)
+![PRC (ts-tr)](PRC-tstr.png)
--- testing/phase-response-curve.h
+++ testing/phase-response-curve.h
@@ -1,154 +1,154 @@
 /*
-   Copyright (C) 2011 Georgia Institute of Technology
+Copyright (C) 2011 Georgia Institute of Technology
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-#include "scatterplot.h"
-#include "runningstat.h"
-#include "RTXIprintfilter.h"
+#include <scatterplot.h>
+#include <runningstat.h>
+//#include <plotdialog.h>
+//#include <RTXIprintfilter.h>
 #include <default_gui_model.h>
 #include <cstdlib>
 #include <QtGui>
 //using namespace std;
 
 class PRC : public DefaultGUIModel {
-
-    Q_OBJECT // macro needed if slots are implemented
-
-public:
-    PRC(void);
-    virtual ~PRC(void);
-    void execute(void);
-    void createGUI(DefaultGUIModel::variable_t *, int);
-    void customizeGUI(void);
-
-    enum mode_t {
-        PRC_BASELINE, // measuring intrinsic P0
-        PRC_PRE, // beginning of protocol, count 10 periods
-        PRC_POST, // after perturbed period
-        PRC_DONE,
-    };
-
-    enum extra_flags_t {
-        STARTBASELINE,
-        MEASUREPRC,
-    };
-
-public slots:
-
-signals:
-    void setPlotRange(double xmin, double xmax, double ymin, double ymax);
-    void saveOK(bool);
-    void setPlotMode(bool);
-    void drawData(double* x, double* y, int size);
-    void drawLine(double* x, double* y, int size);
-
-protected:
-    virtual void update(DefaultGUIModel::update_flags_t);
-    void update(PRC::extra_flags_t);
-
-private:
-    // inputs, states, calculated values
-    double systime;
-    double Vm;
-    double dt;
-    // parameters
-    double mindelay;
-    double maxdelay;
-    double newmindelay;
-    double newmaxdelay;
-    double stepsize; // step minphase and maxphase
-    double gmax; // maximum conductance for alpha shaped perturbation
-    double tau; // time constant
-    double esyn; // synaptic reversal potential
-    double repeat;
-    // options
-    bool randomize;
-    bool plotPRC;
-    bool saveData;
-    mode_t mode;
-    //bookkeeping
-    double spktime;
-    double prevspktime;
-    int nstep;
-    double ISI; // = spktime - prevspktime
-    double P0; // intrinsic period
-    double P1; // period of perturbed cycle
-    double P2; // period of cycle following perturbed cycle
-    bool random; // is shuffled?
-    double periodnum;
-    int Gsyncount;
-    int stepcount; // which step within cycle
-    int cyclecount; // number of repeated cycles
-    long long count; // keep track of time
-    int spikecount;
-    int nidx; // arbitrary idx
-
-    RunningStat runningPeriod;
-
-    double* arrGsyn; // holds the stimulus conductance values
-    double* arrISI; // holds five periods for averaging
-    std::vector<double> reftr; // hold values for reference tr-ts plots
-    std::vector<double> refts;
-
-    double* delays; // holds delays for each protocol
-    // these hold the data to be plotted until it is manually cleared
-    std::vector<double> arrtr; // milliseconds
-    std::vector<double> arrts; // holds delays in order for each cycle in seconds
-    std::vector<double> arrP0; // milliseconds
-    std::vector<double> arrphi; // holds phases, unitless
-    std::vector<double> arrPRC1; // store 1st order PRC, unitless
-    std::vector<double> arrPRC2; // store 2nd order PRC, unitless
-
-    // Spike Detector parameters for cell
-    double thresh;
-    double min_int;
-    int spikestate;
-    double last_spike;
-
-    // PRC functions
-    void initDelayArray(); // initialize stimulus delays
-    void initParameters(); // initialize parameters
-    void initStimulus(); // initialize stimulus conductance waveform based on parameters
-    void bookkeep(); // reset bookkeeping variables
-    void countspikes(); // increment spikes, cycles, compute ISI
-    void resetcycle(); // bookkeeping
-    void shuffleonce(); // shuffle stimulus delays once per cycle
-    double shiftstimulus(double spiketime); // determine correct output amplitude
-    void makeplot();
-//    void drawRefLines();
-//    void LoadRefData(QString);
-
-    // QT components
-    ScatterPlot *splot;
-    QLabel *moduleStatus;
-    QHButtonGroup *plotGroup;
-
-    // Functions and parameters for saving data to file without using data recorder
-    bool OpenFile(QString);
-    QFile dataFile;
-    QTextStream stream;
-
-private slots:
-    // custom
-    void togglerandom(bool);
-    void togglePlot(bool);
-    void savePRC();
-    void startBaseline();
-    void measurePRC();
-    void print();
-    void exportSVG();
-    void clearData();
+	Q_OBJECT // macro needed if slots are implemented
+	
+	public:
+		PRC(void);
+		virtual ~PRC(void);
+		void execute(void);
+		void createGUI(DefaultGUIModel::variable_t *, int);
+		void customizeGUI(void);
+		
+		enum mode_t {
+			PRC_BASELINE, // measuring intrinsic P0
+			PRC_PRE, // beginning of protocol, count 10 periods
+			PRC_POST, // after perturbed period
+			PRC_DONE,
+		};
+		
+		enum extra_flags_t {
+			STARTBASELINE,
+			MEASUREPRC,
+		};
+	
+	public slots:
+	
+	signals:
+		void setPlotRange(double xmin, double xmax, double ymin, double ymax);
+		void saveOK(bool);
+		void setPlotMode(bool);
+		void drawData(double* x, double* y, int size);
+		void drawLine(double* x, double* y, int size);
+	
+	protected:
+		virtual void update(DefaultGUIModel::update_flags_t);
+		void update(PRC::extra_flags_t);
+	
+	private:
+		// inputs, states, calculated values
+		double systime;
+		double Vm;
+		double dt;
+		// parameters
+		double mindelay;
+		double maxdelay;
+		double newmindelay;
+		double newmaxdelay;
+		double stepsize; // step minphase and maxphase
+		double gmax; // maximum conductance for alpha shaped perturbation
+		double tau; // time constant
+		double esyn; // synaptic reversal potential
+		double repeat;
+		// options
+		bool randomize;
+		bool plotPRC;
+		bool saveData;
+		mode_t mode;
+		//bookkeeping
+		double spktime;
+		double prevspktime;
+		int nstep;
+		double ISI; // = spktime - prevspktime
+		double P0; // intrinsic period
+		double P1; // period of perturbed cycle
+		double P2; // period of cycle following perturbed cycle
+		bool random; // is shuffled?
+		double periodnum;
+		int Gsyncount;
+		int stepcount; // which step within cycle
+		int cyclecount; // number of repeated cycles
+		long long count; // keep track of time
+		int spikecount;
+		int nidx; // arbitrary idx
+		
+		RunningStat runningPeriod;
+		
+		double* arrGsyn; // holds the stimulus conductance values
+		double* arrISI; // holds five periods for averaging
+		std::vector<double> reftr; // hold values for reference tr-ts plots
+		std::vector<double> refts;
+		
+		double* delays; // holds delays for each protocol
+		// these hold the data to be plotted until it is manually cleared
+		std::vector<double> arrtr; // milliseconds
+		std::vector<double> arrts; // holds delays in order for each cycle in seconds
+		std::vector<double> arrP0; // milliseconds
+		std::vector<double> arrphi; // holds phases, unitless
+		std::vector<double> arrPRC1; // store 1st order PRC, unitless
+		std::vector<double> arrPRC2; // store 2nd order PRC, unitless
+		
+		// Spike Detector parameters for cell
+		double thresh;
+		double min_int;
+		int spikestate;
+		double last_spike;
+		
+		// PRC functions
+		void initDelayArray(); // initialize stimulus delays
+		void initParameters(); // initialize parameters
+		void initStimulus(); // initialize stimulus conductance waveform based on parameters
+		void bookkeep(); // reset bookkeeping variables
+		void countspikes(); // increment spikes, cycles, compute ISI
+		void resetcycle(); // bookkeeping
+		void shuffleonce(); // shuffle stimulus delays once per cycle
+		double shiftstimulus(double spiketime); // determine correct output amplitude
+		void makeplot();
+		//    void drawRefLines();
+		//    void LoadRefData(QString);
+		
+		// QT components
+		ScatterPlot *splot;
+		QLabel *moduleStatus;
+		QButtonGroup *plotGroup;
+		
+		// Functions and parameters for saving data to file without using data recorder
+		bool OpenFile(QString);
+		QFile dataFile;
+		QTextStream stream;
+	
+	private slots:
+		// custom
+		void togglerandom(bool);
+		void togglePlot(bool);
+		void savePRC();
+		void startBaseline();
+		void measurePRC();
+		void print();
+		void exportSVG();
+		void clearData();
 };
